4cb30b169fae62ea4ac2dec7b57f94d61fa5a393
unknown <mekcikahez@gmail.com>
2020-04-22 15:08:57 +0300

    my discord bot

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = 'NzAyMjQ3OTA4Mzg0NDQwNDA0.Xp_eEQ.30pscPvZ8uQRf5lo_19OH_0PWIE' #bot TOKEN
+
+POST_ID = 702250392825888909 #post id to read reactions from
+
+# roles list according to emotes
+ROLES = {
+	'😃':702249587934756875 , #лох
+	'😄':702249594259636386 , #чепуха 
+	'😅':702249596889464883 , #гнида
+	
+
+}
+
+# exclude this roles from counting
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 15 # max ammount of roles a user can have
